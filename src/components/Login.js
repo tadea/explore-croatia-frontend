c3ff48ecd3e21c1138f8db8d62c55e3900267927
Tadea <tsimunoviczg@gmail.com>
2019-09-08 14:16:50 -0400

    add LoginForm component

---
+++ src/components/Login.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { connect } from "react-redux";
+import { updateLoginForm } from "../actions/loginForm.js";
+import { login } from "../actions/currentUser.js";
+
+const Login = ({ loginFormData, updateLoginForm, login }) => {
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    const updatedFormInfo = {
+      ...loginFormData,
+      [name]: value
+    };
+    updateLoginForm(updatedFormInfo);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    login(loginFormData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="name"
+        value={loginFormData.name}
+        name="name"
+        type="text"
+        onChange={handleInputChange}
+      />
+      <input
+        placeholder="password"
+        value={loginFormData.password}
+        name="password"
+        type="text"
+        onChange={handleInputChange}
+      />
+      <input type="submit" value="Log In" />
+    </form>
+  );
+};
+
+// this gives me an argument coming to this component that looks like this:
+// {
+//   username: "tadSim",
+//   password: "password"
+// }
+const mapStateToProps = state => {
+  return {
+    loginFormData: state.loginForm
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { updateLoginForm, login }
+)(Login);
+
+//video(3) 43'
