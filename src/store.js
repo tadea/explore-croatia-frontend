140e25572becfcf5d24b6a129e35ea482b3aef78
Tadea <tsimunoviczg@gmail.com>
2019-09-16 10:24:29 -0400

    add favBeaches to store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import usersReducer from "./reducers/users.js";
 import currentUser from "./reducers/currentUser.js";
 import loginForm from "./reducers/loginForm.js";
+import favBeaches from "./reducers/favBeaches.js";
 import thunk from "redux-thunk";
 
 const reducer = combineReducers({
   users: usersReducer,
   currentUser,
-  loginForm
+  loginForm,
+  favBeaches
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
