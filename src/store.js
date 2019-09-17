30fd9bae72ffd9ac12d467fa235533e2002ff359
Tadea <tsimunoviczg@gmail.com>
2019-09-16 23:32:51 -0400

    add signup form reducer and add to store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import usersReducer from "./reducers/users.js";
 import currentUser from "./reducers/currentUser.js";
 import loginForm from "./reducers/loginForm.js";
+import signupForm from "./reducers/signupForm.js";
 import favBeaches from "./reducers/favBeaches.js";
 import thunk from "redux-thunk";
 
@@ -9,7 +10,8 @@ const reducer = combineReducers({
   users: usersReducer,
   currentUser,
   loginForm,
-  favBeaches
+  favBeaches,
+  signupForm
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
