1490599ec6ea420fbc8274a9b289d0dbad567054
Tadea <tsimunoviczg@gmail.com>
2019-09-16 23:33:31 -0400

    wrap App in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import { connect } from "react-redux";
 import { getCurrentUser } from "./actions/currentUser.js";
 import NavBar from "./components/NavBar.js";
+import Login from "./components/Login.js";
+import Signup from "./components/Signup.js";
+import Logout from "./components/Logout.js";
+import FavBeaches from "./components/FavBeaches.js";
 import MainContainer from "./components/MainContainer.js";
+import { Route } from "react-router-dom";
 
 class App extends React.Component {
   componentDidMount() {
@@ -13,7 +18,10 @@ class App extends React.Component {
     return (
       <div>
         <NavBar />
-        <MainContainer />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/logout" component={Logout} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/fav-beaches" component={FavBeaches} />
       </div>
     );
   }
